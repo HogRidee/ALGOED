ee9d8e5f7220bece322318040e2720e13fbfc034
HogRidee <iaraoz061102@gmail.com>
2024-03-21 11:29:37 -0500

    Add "MochilaValor" files to class 01

---
+++ Clases/01/MochilaValor/funciones.hpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   funciones.hpp
+ * Author: Ivan
+ *
+ * Created on 21 de marzo de 2024, 10:55
+ */
+
+using namespace std;
+
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+
+#define nObjetos 5
+
+#ifndef FUNCIONES_HPP
+#define FUNCIONES_HPP
+
+void cargaBinaria(int, int *);
+
+#endif /* FUNCIONES_HPP */
